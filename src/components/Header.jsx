9af9c6b0b6ac01706f71735ec9b340c1065785c9
CrisRendon <crizthianrendon@gmail.com>
2023-09-06 21:49:21 -0300

    change header bg-color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = ({ login }) => {
 	return (
 		<>
 			{login && (
-				<header className='fixed bottom-0 z-99 w-full py-3 px-5 flex flex-wrap items-center bg-blue-950'>
+				<header className='fixed bottom-0 z-99 w-full py-3 px-5 flex flex-wrap items-center bg-slate-950'>
 					<Link
 						to={login ? '/home' : '/'}
 						className='font-Righteous text-4xl mx-auto text-white cursor-pointer'>
